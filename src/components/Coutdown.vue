52ded9b8a74c9c9fd3b415e5cca61e3b02fa9349
Ryan <ryantupo98@gmail.com>
2021-05-14 23:24:04 +0100

    14.05.2021 mounted kinda works but doesnt update

--- src/components/Coutdown.vue
+++ src/components/Coutdown.vue
@@ -10,15 +10,25 @@ export default {
   props: {
     totalTime: Number
   },
+  methods: {
+    upDate() {
+      this.amountOfDays = this.totalTime % 86400;
+      this.reminderAfterDays = Math.floor(this.totalTime / 86400);
+      this.amountOfHours = this.reminderAfterDays % 3600;
+      this.reminderAfterHours = Math.floor(this.reminderAfterDays / 3600);
+      this.amountOfMinutes = this.reminderAfterHours % 60;
+      this.remainingMinutes = Math.floor(this.reminderAfterHours / 60);
+    }
+  },
   data() {
     return {
       timerCount: this.totalTime,
-      amountOfDays: this.totalTime % 86400,
-      reminderAfterDays: Math.floor(this.totalTime / 86400),
-      amountOfHours: this.reminderAfterDays % 3600,
-      reminderAfterHours: this.reminderAfterDays / 3600,
-      amountOfMinutes: this.reminderAfterHours % 60,
-      remainingMinutes: this.reminderAfterHours / 60,
+      amountOfDays: 0,
+      reminderAfterDays: 0,
+      amountOfHours: 0,
+      reminderAfterHours: 0,
+      amountOfMinutes: 0,
+      remainingMinutes: 0
     };
   },
 
@@ -28,12 +38,13 @@ export default {
         if (value > 0) {
           setTimeout(() => {
             this.timerCount--;
+            upDate();
             console.log(this.timerCount);
-            console.log(this.reminderAfterDays)
+            console.log(this.reminderAfterDays);
             console.log("days left :", this.amountOfDays);
-            console.log("Hours Left :" , this.amountOfHours);
+            console.log("Hours Left :", this.amountOfHours);
             console.log("minutes Left :", this.amountOfMinutes);
-            console.log("Seconds Left :" , this.remainingMinutes);
+            console.log("Seconds Left :", this.remainingMinutes);
           }, 1000);
         } else if (value === 0) {
           console.log("finished");
